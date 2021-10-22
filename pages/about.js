af1b2e708059c7fb3ad4a0d7cec15e5e280181fc
An Pham <anphamimpala@gmail.com>
2021-10-22 11:43:15 +0700

    Layout, header & footer

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+export default function AboutPage() {
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <p>This is an app to find ....</p>
+      <p>version: 1.0.0</p>
+    </Layout>
+  );
+}
